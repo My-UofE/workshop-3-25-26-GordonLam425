e5d80ce4ed8fb729e0bcb165f32f685d587ab661
GordonLam425 <ll745@exeter.ac.uk>
2026-02-03 23:30:14 +0000

    PrintPattern1 completed

---
+++ PrintPattern1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // We use the Scanner class from java.util to handle user input
+
+public class PrintPattern1 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the size: ");
+        int size = in.nextInt();
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
