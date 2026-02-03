76b816b4dabe035e8c377f66f020d52346bab3ee
GordonLam425 <ll745@exeter.ac.uk>
2026-02-03 22:59:57 +0000

    SumSquares completed with for loop

---
+++ SumSquares.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; // We use the Scanner class from java.util to handle user input
+
+public class SumSquares {
+    public static void main(String[] args) {
+
+        // Declare Scanner object
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the start value: ");
+        int startNum = in.nextInt(); // Reads in an int from standard input
+        
+        System.out.print("Please enter the end value: ");
+        int endNum = in.nextInt();
+
+        in.nextLine();
+
+        System.out.println("Sum of squares from " + startNum + " to " + endNum);
+        
+        int sum = 0;
+
+        for (int i = startNum; i <= endNum; i++) {
+            sum += i * i;
+        }
+        System.out.println(sum);
+    }
+}
